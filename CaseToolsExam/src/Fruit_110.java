a1f4f93a7b0306a30423cf2855e0348befb1169f
Dell <Dell@DESKTOP-RS3V3QB>
2021-07-23 13:58:58 +0530

    First Commit

---
+++ CaseToolsExam/src/Fruit_110.java
@@ -0,0 +1,15 @@
+
+public class Fruit_110 {
+	
+	String color;
+	Fruit_110(String color)
+	{
+	this.color=color;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
